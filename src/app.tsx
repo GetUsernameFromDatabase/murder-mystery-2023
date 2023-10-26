3aa81390f44204fed75dbe10db236f072dc44f79
Ryan Kruberg <rkruberg@gmail.com>
2023-10-26 09:17:51 +0300

    feat: dark theme fix: some linting problems

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { SiteHeader } from "./components/site-header";
+import { ThemeProvider } from "./components/theme/theme-provider";
 import { mainRoutes } from "./configuration/router";
 
 function App() {
   return (
     <BrowserRouter>
-      <main>
-        <SiteHeader></SiteHeader>
-        <Routes>
-          {mainRoutes.map((route, index) => (
-            <Route key={index} {...route} />
-          ))}
-        </Routes>
-      </main>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <main>
+          <SiteHeader></SiteHeader>
+          <Routes>
+            {mainRoutes.map((route, index) => (
+              <Route key={index} {...route} />
+            ))}
+          </Routes>
+        </main>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,15 +1,26 @@
 import { NavigationMain } from "./navigation/navigation-main";
+import { ModeToggle } from "./theme/mode-toggle";
 
 import { siteConfig } from "@/configuration/site";
 
 export function SiteHeader() {
   return (
-    <div className="grid">
-      <header className="sticky top-0 z-40 w-full border-b bg-background">
-        <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-          <NavigationMain items={siteConfig.mainNav} />
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center">
+        <NavigationMain items={siteConfig.mainNav} />
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          <ModeToggle />
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
+  // return (
+  //   <div className="grid">
+  //     <header className="sticky top-0 z-40 w-full border-b bg-background">
+  //       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+  //         <NavigationMain items={siteConfig.mainNav} />
+  //       </div>
+  //     </header>
+  //   </div>
+  // );
 }
